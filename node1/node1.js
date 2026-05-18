3558c553e724828ed92266b385321190bf07e7c8
Arthur-WhiteCorp <arthursilvamatias@gmail.com>
2026-05-18 19:43:20 -0400

    coisa

--- node1/node1.js
+++ node1/node1.js
@@ -1,20 +1,29 @@
-const http = require('http');
-const fs = require('fs')
-require('dotenv').config();
-
-
+const http = require("http");
+const fs = require("fs");
+require("dotenv").config();
 
 const PORT = process.env.PORT ?? 3333;
 
-
 const server = http.createServer(function (req, res) {
-res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
-res.write("Instituto de Computação");
-res.end();
+  res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+  process.argv.forEach((val, index) => {
+    if (index === 2) {
+      fs.readdir(val, null, (err, files) => {
+        if (err) {
+          console.log(`Error reading dir: ${err}`); // Just log it
+          filesList = []; // Set to empty array as fallback
+          return;
+        }
+        console.log(files);
+
+        files.forEach((file, index) => {
+          res.write(`${file.toString()} <br>`);
+        });
+
+        res.end();
+      });
+    }
+  });
 });
 
-
-process.argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`)
-    })
 server.listen(PORT);
